ed94a1a2c30f4503922026976b5591d271be812d
Jamason Davis <jpdavis2471@gmail.com>
2024-10-09 00:15:43 -0500

    Hopefully working data import

--- projects/importList.js
+++ projects/importList.js
@@ -1,7 +1,11 @@
 function loadData() {
+    o;
+
     fetch('./list.json')
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((json) => o = json);
+
+    return o;
 }
 
 function main() {
