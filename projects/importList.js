c71f49cea2a29faaecd9c1a79adaa9b63b5f92ae
Jamason Davis <jpdavis2471@gmail.com>
2024-10-09 00:12:34 -0500

    Testing loading JSON

--- projects/importList.js
+++ projects/importList.js
@@ -1,1 +1,9 @@
-function 
+function loadData() {
+    fetch('./list.json')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+
+function main() {
+    loadData();
+}
