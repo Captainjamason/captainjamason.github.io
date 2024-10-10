eb3a29b646485102618efc3694b21066f9dd3ed3
Jamason Davis <jpdavis2471@gmail.com>
2024-10-09 21:03:41 -0500

    Blog is working.

--- blog/loadPosts.js
+++ blog/loadPosts.js
@@ -2,13 +2,19 @@ async function loadPosts() {
     const cont = document.getElementById("postContainer");
     const post = document.createElement('div');
 
-    const resp = await fetch('./posts/posts.html')
+    const resp = await fetch('./posts.json')
+    const posts = await resp.json();
 
-    const posts = await resp.text();
-
-    post.className = "posts";
-    post.innerHTML = "<h3>Test post added from JS</h3>";
-    cont.appendChild(post);
+    var i = 0;
+    while(i < posts.length) {
+        post.className = "posts";
+        post.innerHTML += `<h3>${posts[i].name}</h3>`;
+        post.innerHTML += `<p>${posts[i].date}</h3>`;
+        post.innerHTML += `<p>${posts[i].content}</p>`;
+        post.innerHTML += `<hr>`
+        cont.appendChild(post);
+        i++;
+    }
 }
 
 loadPosts();
