d93847543763fbe2bba6c058417f142fc84d21b2
Jamason Davis <jpdavis2471@gmail.com>
2024-10-09 20:51:39 -0500

    Working project list fetching, Will impliment for the blog here shortly

--- blog/loadPosts.js
+++ blog/loadPosts.js
@@ -1,11 +1,13 @@
-function loadPosts() {
+async function loadPosts() {
     const cont = document.getElementById("postContainer");
     const post = document.createElement('div');
 
-    fetch('/posts')
+    const resp = await fetch('./posts/posts.html')
+
+    const posts = await resp.text();
 
     post.className = "posts";
-    post.innerHTML = "<h3>Test</h3>";
+    post.innerHTML = "<h3>Test post added from JS</h3>";
     cont.appendChild(post);
 }
 
--- projects/importList.js
+++ projects/importList.js
@@ -1,9 +1,20 @@
-function loadData() {
-    var o = require('./data.json');
+async function impList() {
+    const resp = await fetch('./list.json')
+    const data = await resp.json();
+
+    const cont = document.getElementById("container");
+    const post = document.createElement('div');
+
+    var i = 0;
+    while(i < data.length) {
+        post.innerHTML += `<h2>${data[i].name}</h2>`;
+        post.innerHTML += `<p>Version: ${data[i].ver}</p>`;
+        post.innerHTML += `<p>${data[i].desc}</p>`
+        post.innerHTML += `<a href=${data[i].redir}>Click Here!</a>`
+        post.innerHTML += `<hr>`
+        cont.appendChild(post);
+        i++;
+    }
 }
 
-function main() {
-    var data = loadData();
-    console.log(data);
-    console.log(data[0]);
-}
+impList();
